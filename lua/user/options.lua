1c8c0e163cfe4aa96b18e0a3ec52337a1908f1bd
lllzt <lllzt@pm.me>
2024-12-06 22:30:45 +0800

    delete the options part in init.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -24,10 +24,14 @@ vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
+
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -96,8 +100,33 @@ vim.opt.cursorline = true
 --long lines wrap and continue on the next line
 vim.opt.wrap = false
 
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
 --minimum nr. of lines above and below cursor
 vim.opt.scrolloff = 8
 
+-- Sets how neovim will display certain whitespace characters in the editor. See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
 --min. nr. of columns to left and right of cursor
 vim.opt.sidescrolloff = 8
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
